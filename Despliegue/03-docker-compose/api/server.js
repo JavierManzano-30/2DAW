993d3801b6d8f8b80c7d77a01bd948159aecf9d7
JavierManzano-30 <javier.manzanooliveros@adaits.es>
2025-11-10 17:15:26 +0100

    Despliegue-Dockercompose

--- Despliegue/03-docker-compose/api/server.js
+++ Despliegue/03-docker-compose/api/server.js
@@ -7,10 +7,9 @@ const port = 3000;
 app.use(cors());
 
 app.get('/api/adaitsdawdocker', (req, res) => {
-    res.json({ message: '¡Hola desde la API!' });
+    res.json({ message: 'Hola desde la API!' });
 });
 
 app.listen(port, () => {
     console.log(`API escuchando en el puerto ${port}`);
 });
-
